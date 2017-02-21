304f78e00a46aa18dbfb0f224b8ee05b74380259
roboticsteam <jjohnson18@gssm.k12.sc.us>
2017-02-21 18:58:19 -0500

    Developed VisionTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experimental/VisionTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experimental/VisionTeleOp.java
@@ -1,11 +1,48 @@
 package org.firstinspires.ftc.teamcode.experimental;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.lasarobotics.vision.opmode.VisionOpMode;
 
 /**
  * Created by roboticsteam on 2/21/2017.
  */
-
+@TeleOp(name = "Vision Teleop")
 public class VisionTeleOp extends VisionOpMode {
+    VisionRobot VisionBot;
+    private final FinalTeleOp;
+    /**
+     * Tank Drive System Description
+     *
+     * Gamepad 1 (Driver):
+     *      Left Joystick y: Left Drive
+     *      Right Joystick y: Right Drive
+     *
+     *      A:
+     *          Flipper down
+     *      B:
+     *          Flipper up
+     *
+     *
+     * Gamepad 2 (Operator):
+     *      Triggers:
+     *          Right (Shoot direction)
+     *          Left (Reverse)
+     *          (Right - Left is direction)
+     *
+     *
+     *
+     */
 
+
+
+
+    @Override
+    public void loop()
+    {
+        FinalTeleOp Teleop;
+
+        Teleop.loop();
+
+    }
 }
