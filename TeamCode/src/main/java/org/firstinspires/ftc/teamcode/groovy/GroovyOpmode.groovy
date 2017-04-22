8b8ac75b1ec7612cc6cd4f4580f35104a83fc317
Hunter Damron <hdamron1594@yahoo.com>
2017-04-22 14:23:22 -0400

    Groovy works fairly quickly, but Scala has dependency trouble and Jython appears to work but doesn't.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/groovy/GroovyOpmode.groovy
@@ -0,0 +1,11 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+
+class GrooyOpmode extends OpMode {
+    void init() {
+
+    }
+
+    void loop() {
+        telemetry.addData("Status", "Groovy")
+    }
+}
